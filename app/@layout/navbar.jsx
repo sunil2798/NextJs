189e7b9bc3537897de50a0620a2ac92b3a97edcb
sunil2798 <sunil.saini@deeporion.com>
2023-12-19 17:18:09 +0530

    hot fixes

--- app/@layout/navbar.jsx
+++ app/@layout/navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Image from 'next/image'
+import examimg from './image/examimg.jpg';
+import img from './image/img.jpg';
 
 const Navbar = () => {
   return (
@@ -7,14 +10,15 @@ const Navbar = () => {
         height: "20%",
         backgroundColor: "orange",
         display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "space-evenly",
         padding: "20px",
       }}
     >
-      <div style={{width:"30%"}}>
+      <div style={{ width: "30%", display:"flex", }}>
+        <Image src={img} alt="logo"style={{ height: "50px", width: "50px",borderRadius:"10px", marginRight:"10px"}}/>
         <h2>Logo</h2>
       </div>
-      <div className="navDiv"  style={{display: "flex", flexWrap:"nowrap", justifyContent: "space-between", width:"60%"}}>
+      <div className="navDiv" style={{ display: "flex", flexWrap: "nowrap", justifyContent: "space-between", width: "60%" }}>
         <a href="/" className="navLink" id="div1">Home</a>
         <a href="/" className="navLink" id="div2">Coures</a>
         <a href="/" className="navLink" id="div3">About us</a>
--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Dashboard from "./@layout/Dashboard";
 import { Button } from "@mui/material";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Tooltip from '@mui/material/Tooltip'
 
 export default function Home() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -22,6 +24,7 @@ export default function Home() {
   return (
     <>
       <div style={{ display: "flex" }}>
+
         <div
           style={{
             height: "100vh",
@@ -32,195 +35,221 @@ export default function Home() {
             flexDirection: "column",
             textAlign: "left",
             gap: "10px",
-            position:"fixed",
+            position: "fixed",
             zIndex: "1",
           }}
         >
+          <div style={{ display: "flex" }}>
+            <Tooltip title="Dashboard" placement="top" arrow>
+              <DashboardIcon style={{ color: "#FFF", alignSelf: "center" }} />
+            </Tooltip>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", whiteSpace: "nowrap", textTransform: "capitalize", marginLeft: "10px" }}
+            >
+              Dashboard
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+              style={{ alignSelf: "end" }}
+            >
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Profile
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  My Account
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Logout
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
 
-          <Button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", whiteSpace: "nowrap", flexShrink: "inherit", }}
-          >
-            Dashboard
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-            style={{ alignSelf: "end" }}
-          >
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Profile
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                My Account
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Logout
-              </a>
-            </MenuItem>
-          </Menu>
+          <div style={{ display: "flex" }}>
+            <Tooltip title=" Super Admin" placement="top" arrow>
+              <DashboardIcon style={{ color: "#FFF", alignSelf: "center" }} />
+            </Tooltip>
+            <Button
+              id="basic-Button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", whiteSpace: "nowrap", textTransform: "capitalize", marginLeft: "10px" }}
+            >
+              Super Admin
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-Button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Profile
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  My Account
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Logout
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
 
-          <Button
-            id="basic-Button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ fontSize: "20px", color: "#FFF", alignSelf: "start" }}
-          >
-            Super Admin
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-Button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Profile
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                My Account
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Logout
-              </a>
-            </MenuItem>
-          </Menu>
+          <div style={{ display: "flex" }}>
+            <Tooltip title="Admin" placement="top" arrow>
+              <DashboardIcon style={{ color: "#FFF", alignSelf: "center" }} />
+            </Tooltip>
+            <Button
+              id="basic-Button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", textTransform: "capitalize", marginLeft: "10px" }}
+            >
+              Admin
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-Button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Profile
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  My Account
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Logout
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
 
-          <Button
-            id="basic-Button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ fontSize: "20px", color: "#FFF", alignSelf: "start" }}
-          >
-            Admin
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-Button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Profile
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                My Account
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Logout
-              </a>
-            </MenuItem>
-          </Menu>
+          <div style={{ display: "flex" }}>
+            <Tooltip title="Student Name" placement="top" arrow>
+              <DashboardIcon style={{ color: "#FFF", alignSelf: "center" }} />
+            </Tooltip>
+            <Button
+              id="basic-Button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", whiteSpace: "nowrap", overflow: "hidden", textTransform: "capitalize", marginLeft: "10px" }}
+            >
+              Student Name
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-Button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Profile
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  My Account
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Logout
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
 
-          <Button
-            id="basic-Button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", whiteSpace: "nowrap", }}
-          >
-            Student Name
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-Button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Profile
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                My Account
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Logout
-              </a>
-            </MenuItem>
-          </Menu>
-          <Button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            style={{ fontSize: "20px", color: "#FFF", alignSelf: "start" }}
-          >
-            Status
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Profile
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                My Account
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" style={{ textDecoration: "none", color: "#000" }}>
-                Logout
-              </a>
-            </MenuItem>
-          </Menu>
+          <div style={{ display: "flex" }}>
+            <Tooltip title="Status" placement="top" arrow>
+              <DashboardIcon style={{ color: "#FFF", alignSelf: "center" }} />
+            </Tooltip>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              style={{ fontSize: "20px", color: "#FFF", alignSelf: "start", textTransform: "capitalize", marginLeft: "10px" }}
+            >
+              Status
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Profile
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  My Account
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="/" style={{ textDecoration: "none", color: "#000" }}>
+                  Logout
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
+
         </div>
 
-        <div style={{ height:"100vh" , width: "100vw", padding: "20px", marginLeft:"15%"}}>
+        <div style={{ height: "100vh", width: "100vw", padding: "20px", marginLeft: "15%" }}>
           <h1>Main Page</h1>
-          <Dashboard/>
+          <Dashboard />
         </div>
 
       </div>
